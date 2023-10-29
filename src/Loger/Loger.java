e1a1d996719f64bf02ed1c4b27418c366871bfa6
Konstantin <pentaplast@rambler.ru>
2023-10-29 11:07:51 +0300

    Test

--- src/Loger/Loger.java
+++ src/Loger/Loger.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 import java.util.Date;
 
 public class Loger {
-    public void loger1 (String s, ComplexNumber result) throws IOException {
-        try (FileWriter fileWriter = new FileWriter("ComplexCalc.txt", true)){
-                fileWriter.append(new Date()+ "\nВыполнена математическая операция: " + s + "\n" + "Результат: " + result + "\n\r");
+    public void loger1(String s, ComplexNumber result) throws IOException {
+        try (FileWriter fileWriter = new FileWriter("ComplexCalc.txt", true)) {
+            fileWriter.append(new Date() + "\nВыполнена математическая операция: " + s + "\n" + "Результат: " + result + "\n\r");
         }
     }
 }
